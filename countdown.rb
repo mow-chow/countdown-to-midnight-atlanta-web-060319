878540402b6efd43058102bcb6a7e820ab5f4112
Catherine Cherubin <cat.cherubin@gmail.com>
2019-05-10 19:13:42 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    break if number == 0
+      puts "HAPPY NEWYEAR!"
+    end
+    number -= 1
+    sleep 1
+  end
+
 end
